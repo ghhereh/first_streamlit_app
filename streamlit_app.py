dbec74f463077b55764e2b887cb158d4c11bae32
h_i_o <111340436+ghhereh@users.noreply.github.com>
2022-08-19 09:13:46 +0200

    no text on the app anymore for fruityvice_response

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,12 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
+# streamlit.text(fruityvice_response) # just writes the data to the screen
 
 # Course Lesson 9: Streamlit - Using APIs & Variables - Making the JSON Look Good 
 # write your own comment -what does the next line do? show content like a string less readable
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do? show content in a readable table
-# streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
   
