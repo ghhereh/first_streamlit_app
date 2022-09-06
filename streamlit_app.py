e684c55c746bd06d75de23d655b5351758c32c83
h_i_o <111340436+ghhereh@users.noreply.github.com>
2022-09-06 21:16:31 +0200

    Update streamlit_app.py
    query trial account metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,13 @@ streamlit.write('The user entered ', fruit_choice)
 # Ende Lesson 9: Streamlit - Using APIs & Variables Variables in Streamlit
 
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
