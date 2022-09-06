7ec61cc6f2aad50089ce68929810f3bd2d39853a
h_i_o <111340436+ghhereh@users.noreply.github.com>
2022-09-06 15:22:44 +0200

    Update streamlit_app.py
    Add a Text Entry Box

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,13 @@ streamlit.dataframe(fruits_to_show)
 # Let's Call the Fruityvice API from Our Streamlit App!
 # We need to bring in another Python package library. This one is called requests. 
 streamlit.header("Fruityvice Fruit Advice!")
+
+# Start Lesson 9: Streamlit - Using APIs & Variables Variables in Streamlit
+# Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+# Ende Lesson 9: Streamlit - Using APIs & Variables Variables in Streamlit
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
