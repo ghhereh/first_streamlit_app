40ae2b14d314480f6cc196f3f3a57ac023f9f081
h_i_o <111340436+ghhereh@users.noreply.github.com>
2022-09-07 15:16:52 +0200

    Update streamlit_app.py
    again lost in format

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,9 +103,9 @@ def insert_row_snowflake(new_fruit):
 add_my_fruit = streamlit.text_input('What fruit would yu like to add?')
 #########  streamlit.write('Thanks for adding ', add_my_fruit)
 if streamlit.button('Add a Fruit to the List'):
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
-back_from_function = insert_row_snowflake(add_my_fruit) 
-streamlit.text(back_from_function)
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
   
 #  Lesson 12: Streamlit, but with Snowflake Added Time to Tidy Up? 
 # Write Code to Add Rows to Our Fruit List in Snowflake
